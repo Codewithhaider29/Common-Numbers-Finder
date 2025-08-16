3ff5a15a83f9c82c8a05e48e2bc4c17ec7437364
code_with_haider <personalhaider2004@gmail.com>
2025-08-16 00:29:22 -0700

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function findCommon() {
-  // user input ko space se split karna
   let groupA = document.getElementById("groupA").value.trim().split(/\s+/).map(Number);
   let groupB = document.getElementById("groupB").value.trim().split(/\s+/).map(Number);
 
   // common numbers nikalna
   let common = groupA.filter(num => groupB.includes(num));
 
-  // result show karna
-  document.getElementById("result").innerHTML =
-    common.length > 0 ? "Common Numbers: " + common.join(" ") : "No Common Numbers Found!";
+  let resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
+
+  if (common.length > 0) {
+    let table = "<table><tr><th>Common Numbers</th></tr>";
+    common.forEach(num => {
+      table += `<tr><td>${num}</td></tr>`;
+    });
+    table += "</table>";
+    resultDiv.innerHTML = table;
+  } else {
+    resultDiv.innerHTML = "<p>No Common Numbers Found!</p>";
+  }
 }
